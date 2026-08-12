15877900ec4c3becb1ea85c8ed4601a1b6d3f8d1
jfhamel <jfhamel@me.com>
2026-08-11 20:17:59 -0400

    Comment out LISTEN_ONLY macro in broan.h
    Comment out the LISTEN_ONLY macro definition.

--- components/broan/broan.h
+++ components/broan/broan.h
@@ -36,7 +36,7 @@ namespace broan {
 #define FILTER_LIFE_MAX 7884000
 
 //#define SCAN_UNKNOWN 1
-#define LISTEN_ONLY 1
+//#define LISTEN_ONLY 1
 
 template<typename T>
 concept BroanFieldTypes = 	std::is_same_v<T, float> ||
