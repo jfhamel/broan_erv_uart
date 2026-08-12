2c6c10ecea990822ddfbb498dc181cb9092bcd99
Jean-François Hamel <jfhamel@me.com>
2026-08-11 22:55:44 -0400

    no message

--- components/broan/button/__init__.py
+++ components/broan/button/__init__.py
@@ -1,77 +1,30 @@
 import esphome.codegen as cg
-from esphome.components import number
+from esphome.components import button
 import esphome.config_validation as cv
 from esphome.const import (
-    CONF_ID,
-    DEVICE_CLASS_SPEED,
-    DEVICE_CLASS_HUMIDITY,
     ENTITY_CATEGORY_CONFIG,
-    ICON_FAN,
-    ICON_WATER,
-    ICON_TIMER,
-    UNIT_PERCENT,
+    ICON_RESTART,
 )
-
-UNIT_CFM = "CFM"
-UNIT_PERIOD = "Period"
 
 from .. import CONF_BROAN_ID, BroanComponent, broan_ns
 
-FanSpeedNumber = broan_ns.class_("FanSpeedNumber", number.Number)
-HumiditySetpointNumber = broan_ns.class_("HumiditySetpointNumber", number.Number)
-IntermittentPeriodNumber = broan_ns.class_("IntermittentPeriodNumber", number.Number)
+FilterResetButton = broan_ns.class_("FilterResetButton", button.Button)
 
-CONF_FAN_SPEED = "fan_speed"
-CONF_HUMIDITY_SETPOINT = "humidity_setpoint"
-CONF_INT_PERIOD = "intermittent_period"
+CONF_FILTER_RESET = "filter_reset"
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(CONF_BROAN_ID): cv.use_id(BroanComponent),
-        cv.Optional(CONF_FAN_SPEED): number.number_schema(
-            FanSpeedNumber,
-            device_class=DEVICE_CLASS_SPEED,
-            entity_category=ENTITY_CATEGORY_CONFIG,
-			unit_of_measurement=UNIT_PERCENT,
-            icon=ICON_FAN,
-        ),
-        cv.Optional(CONF_HUMIDITY_SETPOINT): number.number_schema(
-            HumiditySetpointNumber,
-            device_class=DEVICE_CLASS_HUMIDITY,
-            entity_category=ENTITY_CATEGORY_CONFIG,
-            unit_of_measurement=UNIT_PERCENT,
-            icon=ICON_WATER,
-        ),
-        cv.Optional(CONF_INT_PERIOD): number.number_schema(
-            IntermittentPeriodNumber,
-            entity_category=ENTITY_CATEGORY_CONFIG,
-            unit_of_measurement=UNIT_PERIOD,
-            icon=ICON_TIMER,
-        )
-    }
-)
+CONFIG_SCHEMA = {
+    cv.GenerateID(CONF_BROAN_ID): cv.use_id(BroanComponent),
+    cv.Optional(CONF_FILTER_RESET): button.button_schema(
+        FilterResetButton,
+        entity_category=ENTITY_CATEGORY_CONFIG,
+        icon=ICON_RESTART,
+    ),
+}
 
 
 async def to_code(config):
     broan_component = await cg.get_variable(config[CONF_BROAN_ID])
-
-    if fan_speed_config := config.get(CONF_FAN_SPEED):
-        n = await number.new_number(
-            fan_speed_config, min_value=0, max_value=100, step=10
-        )
-        await cg.register_parented(n, config[CONF_BROAN_ID])
-        cg.add(broan_component.set_fan_speed_number(n))
-
-    if humidity_setpoint_config := config.get(CONF_HUMIDITY_SETPOINT):
-        h = await number.new_number(
-            humidity_setpoint_config, min_value=30, max_value=55, step=5
-        )
-        await cg.register_parented(h, config[CONF_BROAN_ID])
-        cg.add(broan_component.set_humidity_setpoint_number(h))
-
-    if intermittent_period_config := config.get(CONF_INT_PERIOD):
-        h = await number.new_number(
-            intermittent_period_config, min_value=10, max_value=50000, step=1
-        )
-        await cg.register_parented(h, config[CONF_BROAN_ID])
-        cg.add(broan_component.set_intermittent_period_number(h))
+    if filter_reset_config := config.get(CONF_FILTER_RESET):
+        b = await button.new_button(filter_reset_config)
+        await cg.register_parented(b, config[CONF_BROAN_ID])
+        cg.add(broan_component.set_filter_reset_button(b))
